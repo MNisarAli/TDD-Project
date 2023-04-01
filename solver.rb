b9d8717a61a31b11794dfaca3829a8900269071a
Amaka Konwea <lawrahcollections@gmail.com>
2023-04-01 11:35:34 +0100

    Implement logic for fizzbuzz method

--- solver.rb
+++ solver.rb
@@ -10,6 +10,14 @@ class Solver
   end
 
   def fizzbuzz(num)
-    # to be implemented
+    if (num % 3).zero? && (num % 5).zero?
+      'fizzbuzz'
+    elsif (num % 3).zero?
+      'fizz'
+    elsif (num % 5).zero?
+      'buzz'
+    else
+      num.to_s
+    end
   end
 end
