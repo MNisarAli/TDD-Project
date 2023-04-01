690353b25f2de6f55e280f4fc5443fbcbac6cf4b
M.Nisar Ali <101400298+MNisarAli@users.noreply.github.com>
2023-04-01 09:34:24 +0500

    Create file solver.rb and define Solver class

---
+++ solver.rb
@@ -0,0 +1,13 @@
+class Solver
+  def factorial(num)
+    # to be implemented
+  end
+
+  def reverse(word)
+    # to be implemented
+  end
+
+  def fizzbuzz(num)
+    # to be implemented
+  end
+end
