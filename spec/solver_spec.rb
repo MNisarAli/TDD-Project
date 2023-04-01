b3a8f19cfe1d1ddb5c8dce7daa8a0d07abe2dff5
M.Nisar Ali <101400298+MNisarAli@users.noreply.github.com>
2023-04-01 09:43:49 +0500

    Add tests for factorial method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,1 +1,24 @@
 require_relative '../solver'
+
+describe Solver do
+  describe 'Testing factorial method' do
+    context 'when given 0' do
+      it 'returns 1' do
+        expect(Solver.new.factorial(0)).to eq(1)
+      end
+    end
+
+    context 'when given a positive integer' do
+      it 'returns the correct factorial' do
+        expect(Solver.new.factorial(5)).to eq(120)
+        expect(Solver.new.factorial(10)).to eq(3_628_800)
+      end
+    end
+
+    context 'when given a negative integer' do
+      it 'raises an exception' do
+        expect { Solver.new.factorial(-5) }.to raise_error(ArgumentError)
+      end
+    end
+  end
+end
